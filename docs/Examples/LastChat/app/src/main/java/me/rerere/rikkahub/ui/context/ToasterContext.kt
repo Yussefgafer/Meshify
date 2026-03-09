34567ff713cfe59a6831f2d6bff35507f255c8b4
Yussef Gafer Mohamed <yussrfgafar@gmail.com>
2026-03-09 03:45:06 +0200

    chore: cleanup repository and remove ignored files (Examples, analysis, build artifacts)

--- docs/Examples/LastChat/app/src/main/java/me/rerere/rikkahub/ui/context/ToasterContext.kt
+++
@@ -1,6 +0,0 @@
-package me.rerere.rikkahub.ui.context
-
-import androidx.compose.runtime.staticCompositionLocalOf
-import me.rerere.rikkahub.ui.components.ui.AppToasterState
-
-val LocalToaster = staticCompositionLocalOf<AppToasterState> { error("Not provided") }
